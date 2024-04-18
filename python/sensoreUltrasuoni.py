965c589e6bb1a843660795b6a6047155a81bb68c
F <97744412+S3RO-tonino@users.noreply.github.com>
2024-04-18 13:28:38 +0200

    update del file .py del sensore

--- python/sensoreUltrasuoni.py
+++ python/sensoreUltrasuoni.py
@@ -1,4 +1,4 @@
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
@@ -14,20 +14,24 @@ GPIO.setup(ECHO, GPIO.IN)
 GPIO.output(TRIG, False) # accendo il sensore
 time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001) # invio del segnale
-GPIO.output(TRIG, False)
+try:
+	while(True):
+		GPIO.output(TRIG, True)
+		time.sleep(0.00001) # invio del segnale
+		GPIO.output(TRIG, False)
 
-while(GPIO.input(ECHO) == 0):
-    pulse_start = time.time()
+		while(GPIO.input(ECHO) == 0):
+    			pulse_start = time.time()
 
-while(GPIO.input(ECHO) == 1):
-    pulse_end = time.time()
+		while(GPIO.input(ECHO) == 1):
+    			pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
+		pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
-distance = round(distance, 2)
-print(f"Distanza: {distance}cm")
-
-GPIO.clenup()
+		distance = pulse_duration * 17150
+		distance = round(distance, 2)
+		print(f"Distanza: {distance}cm")
+		time.sleep(3)
+except(KeyboardInterrupt):
+	print("CTRL+C pressed, quitting...")
+	GPIO.cleanup()
