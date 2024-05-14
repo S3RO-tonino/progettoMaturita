9624d1af0b978d39197df193abaa079cae2b191f
F <97744412+S3RO-tonino@users.noreply.github.com>
2024-05-14 20:55:48 +0200

    Add files via upload

--- websocket/sensoreUltrasuoni.py
+++ websocket/sensoreUltrasuoni.py
@@ -14,24 +14,25 @@ time.sleep(2)
 
 def leggiDati():
     try:
-	    while(True):
-		    GPIO.output(TRIG, True)
-		    time.sleep(0.5) # invio del segnale
-		    GPIO.output(TRIG, False)
+        while(True):
+            GPIO.output(TRIG, True)
+            time.sleep(0.5) # invio del segnale
+            GPIO.output(TRIG, False)
 
-		    while(GPIO.input(ECHO) == 0):
-        			pulse_start = time.time()
+            while(GPIO.input(ECHO) == 0):
+                pulse_start = time.time()
 
-		    while(GPIO.input(ECHO) == 1):
-        			pulse_end = time.time()
+            while(GPIO.input(ECHO) == 1):
+                pulse_end = time.time()
 
-		    pulse_duration = pulse_end - pulse_start
+            pulse_duration = pulse_end - pulse_start
 
-		    distance = pulse_duration * 17150
-		    distance = round(distance, 2)
-		    if distance >= 20: ele = "Rilevato qualcosa"
+            distance = pulse_duration * 17150
+            distance = round(distance, 2)
+            if distance >= 20: ele = "Rilevato qualcosa"
             else: ele = "Nessun rilevamento"
     except Exception as e: ele = (f"Errore durante la lettura: {e}")
+    print(ele)
     return ele
 
 
--- websocket/server.py
+++ websocket/server.py
@@ -12,7 +12,7 @@ async def server(websocket, path):
 
         await asyncio.sleep(1)  # Aggiorna i dati ogni secondo
 
-start_server = websockets.serve(server, 'localhost', 8765)
+start_server = websockets.serve(server, '192.168.1.155', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
