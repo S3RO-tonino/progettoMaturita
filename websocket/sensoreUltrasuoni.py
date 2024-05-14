62d94608e1a24f5b31da2ce8be9b2af966a5eb5a
F <97744412+S3RO-tonino@users.noreply.github.com>
2024-05-14 19:23:03 +0200

    Add files via upload

--- websocket/sensoreUltrasuoni.py
+++ websocket/sensoreUltrasuoni.py
@@ -1,6 +1,37 @@
-# sensor_handler.py
-def leggi_dati_sensore():
-    # Qui inserisci il codice per leggere i dati dai tuoi sensori
-    dati = "Dati dai sensori"
-    return dati
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23
+ECHO = 24
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False) # accendo il sensore
+time.sleep(2)
+
+def leggiDati():
+    try:
+	    while(True):
+		    GPIO.output(TRIG, True)
+		    time.sleep(0.5) # invio del segnale
+		    GPIO.output(TRIG, False)
+
+		    while(GPIO.input(ECHO) == 0):
+        			pulse_start = time.time()
+
+		    while(GPIO.input(ECHO) == 1):
+        			pulse_end = time.time()
+
+		    pulse_duration = pulse_end - pulse_start
+
+		    distance = pulse_duration * 17150
+		    distance = round(distance, 2)
+		    if distance >= 20: ele = "Rilevato qualcosa"
+            else: ele = "Nessun rilevamento"
+    except Exception as e: ele = (f"Errore durante la lettura: {e}")
+    return ele
+
 
