db5f70013b77418f5163840357bfdb3259bbf0f6
S3RO-tonino <flaviocavaliere2005@gmail.com>
2024-06-27 20:04:29 +0200

    backend

--- backend/server.py
+++ backend/server.py
@@ -6,17 +6,9 @@ from sensoreUltrasuoni import leggiDati as leggiDatiSU
 from sensoreMagnetico import leggiDati as leggiDatiSM
 
 
-jsonFilePath = "config.json"
 print("Avviando il server...")
 async def server(websocket, path):
     try:
-        def readJson():
-            with open(jsonFilePath, "r", encoding='utf-8') as f: config = json.load(f)
-            return config
-
-        def dumpJson(obj):
-            with open(jsonFilePath, 'w', encoding='utf-8') as f: json.dump(obj, f, ensure_ascii=False) 
-
         def differenza(i, f):
             diff = int(f.replace(":",""))-int(i.replace(":",""))
             return diff
@@ -25,13 +17,10 @@ async def server(websocket, path):
             return datetime.now().strftime('%H:%M:%S')
 
         print("Connessione con il client eseguita")
-        status = "ON" if bool(readJson()["status"]) else "OFF"
-        #print(f"[DEBUG] STATUS: {status}, type: {type(status)}")
-        startMessage = {"ID": 'start', "allarmStatus": f'{status}'}
+
+        startMessage = {"ID": 'start'}
         print(f"SENDING: {startMessage}")
-        #print(f"allarm: {allarm}, type: {type(allarm)}")
         await websocket.send(json.dumps(startMessage))
-        #await websocket.send(json.dumps({"sensorID": 'clientConnected', "data": 'Nessuna intrusione', "time": f"{datetime.now().strftime('%H:%M:%S')}"}))
         end = timeNow()
 
         while True:
@@ -41,32 +30,16 @@ async def server(websocket, path):
             start = timeNow()
 
             # Invia i dati al client tramite WebSocket se sono diversi da ND (no data)
-            if(status):
-                #print(f"DEBUG {datiSM['intrusion']}, {type(datiSM['intrusion'])}")
-                if(datiSU['intrusion']==True or datiSM['intrusion']==True):
-                    #print("DEBUG INTRUSIONE")
-                    if(datiSU['intrusion']==True):
-                        await websocket.send(json.dumps(datiSU))
-                        #print("DEBUG SU")
-                    if(datiSM['intrusion']==True):
-                        await websocket.send(json.dumps(datiSM))
-                        #print("DEBUG SM")
-                    end = timeNow()
-                elif(differenza(end, start)>=5):
-                    #await websocket.send(json.dumps({"ID": 'noIntrusion'}))
+            if(datiSU['intrusion']==True or datiSM['intrusion']==True):
+                if(datiSU['intrusion']==True):
                     await websocket.send(json.dumps(datiSU))
+                if(datiSM['intrusion']==True):
                     await websocket.send(json.dumps(datiSM))
-                    #print("DEBUG NO INTRUSIONE")
-                #print(f"[DEBUG] - DIFFERENZA: {differenza(end, start)} - INIZIO FINE: {start} {end} - SU: {datiSU['intrusion']} - SM:{datiSM['intrusion']} - STATUS {status}")
-                    #if datiSU['intrusion']==True: toSend.append(datiSU['name'])
-                    #if datiSM['intrusion']==True: toSend.append(datiSM['name'])
-                #if(differenza(end, start)>=5):
-                    #toSend.append("noIntrusion")
-                #print(f"[DEBUG] - DIFFERENZA: {differenza(end, start)} - INIZIO FINE: {start} {end} - SU: {datiSU['intrusion']} - SM:{datiSM['intrusion']} - STATUS {status}")
-                #await websocket.send(json.dumps(toSend))
-                #end = timeNow()
-                #print(f"SENDING: {datiSU}")
-                #print(f"SENDING: {datiSM}")
+            elif(differenza(end, start)>=5):
+                await websocket.send(json.dumps(datiSU))
+                await websocket.send(json.dumps(datiSM))
+            end = timeNow()
+
             await asyncio.sleep(1)  # Aggiorna i dati ogni secondo
     except Exception as e: print(f"Errore: {e}")
 
